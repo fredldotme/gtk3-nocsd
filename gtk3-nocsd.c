0f8d71f259f180db98f073e4f93d3012ca2347ec
Yaroslav Sidlovsky <zawertun@gmail.com>
2020-06-13 14:49:16 +0300

    reintroduce deprecated macros G_TYPE_INSTANCE_GET_PRIVATE

--- gtk3-nocsd.c
+++ gtk3-nocsd.c
@@ -39,6 +39,11 @@
 
 #include <gobject/gvaluecollector.h>
 
+#ifdef G_TYPE_INSTANCE_GET_PRIVATE
+#   undef G_TYPE_INSTANCE_GET_PRIVATE
+#endif
+#define G_TYPE_INSTANCE_GET_PRIVATE(instance, g_type, c_type)   ((c_type*) g_type_instance_get_private ((GTypeInstance*) (instance),(g_type)))
+
 typedef void (*gtk_window_buildable_add_child_t) (GtkBuildable *buildable, GtkBuilder *builder, GObject *child, const gchar *type);
 typedef GObject* (*gtk_dialog_constructor_t) (GType type, guint n_construct_properties, GObjectConstructParam *construct_params);
 typedef char *(*gtk_check_version_t) (guint required_major, guint required_minor, guint required_micro);
